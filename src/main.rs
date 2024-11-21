1553562a119f4df178227e8e381f9dbf9be15071
zimrustpro <zimrustpro@gmail.com>
2024-11-21 16:47:32 +0200

    Filtering using iterator methods

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
 fn main() {
-    println!("Hello, world!");
+    let months = vec![
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+    ];
+    let filtered_months = months
+        .into_iter()
+        .filter(|month| month.len() < 5)
+        .filter(|month| month.contains("u"))
+        .collect::<Vec<&str>>();
+    println!("{:?}", filtered_months);
 }
