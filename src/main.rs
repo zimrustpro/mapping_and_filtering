c173246199214fff945723d093e3bf69caf0af3b
zimrustpro <zimrustpro@gmail.com>
2024-11-22 22:34:39 +0200

    Add together items in an iterator using `.fold()`

--- src/main.rs
+++ src/main.rs
@@ -7,4 +7,12 @@ fn main() {
     let skip_then_ten_chars = ('a'..).skip(1_300).take(10).collect::<Vec<char>>();
     println!("{:?}", ten_chars);
     println!("{:?}", skip_then_ten_chars);
+
+    let some_numbers = vec![9, 6, 9, 10, 11];
+    println!(
+        "{}",
+        some_numbers
+            .iter()
+            .fold(0, |total_so_far, next_number| total_so_far + next_number)
+    );
 }
