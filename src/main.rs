3eeabf4f5cff0cceb07648f511ba3ae0074c6d27
zimrustpro <zimrustpro@gmail.com>
2024-11-22 22:16:56 +0200

    Use `.rev()` to reverse the iterator

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use core::num;
+
 fn in_char_vec(char_vec: &Vec<char>, check: char) {
     println!(
         "Is {} inside? {}",
@@ -21,4 +23,26 @@ fn main() {
         "All less than the character 행? {}",
         smaller_vec.iter().all(|&x| x < '행')
     );
+
+    let mut big_vec = vec![6; 1000];
+    big_vec.push(5);
+
+    let mut iterator = big_vec.iter().rev();
+    assert_eq!(iterator.next(), Some(&5));
+    assert_eq!(iterator.next(), Some(&6));
+
+    println!("{:?}", big_vec.iter().rev().any(|&number| number == 5));
+
+    let mut big_vec = vec![6; 1000];
+    big_vec.push(5);
+
+    let mut num_loops = 0;
+    let mut big_iter = big_vec.into_iter();
+    loop {
+        num_loops += 1;
+        if big_iter.next() == Some(5) {
+            break;
+        }
+    }
+    println!("Number of loops: {}", num_loops);
 }
