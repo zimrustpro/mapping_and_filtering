d8f7cb2ecbde0e204f6273be15fef2157052790e
zimrustpro <zimrustpro@gmail.com>
2024-11-22 22:27:11 +0200

    Create iterator using range that doesn't have an ending

--- src/main.rs
+++ src/main.rs
@@ -2,4 +2,9 @@ fn main() {
     let even_odd_iter = ["even", "odd"].into_iter().cycle();
     let even_odd_vec = (0..=5).zip(even_odd_iter).collect::<Vec<(i32, &str)>>();
     println!("{:?}", even_odd_vec);
+
+    let ten_chars = ('a'..).take(10).collect::<Vec<char>>();
+    let skip_then_ten_chars = ('a'..).skip(1_300).take(10).collect::<Vec<char>>();
+    println!("{:?}", ten_chars);
+    println!("{:?}", skip_then_ten_chars);
 }
