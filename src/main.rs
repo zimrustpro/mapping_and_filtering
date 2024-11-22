b28e29c4455cd93bd95056e0c54fef56b1b53d42
zimrustpro <zimrustpro@gmail.com>
2024-11-22 22:46:10 +0200

    Use `by_ref()` to use part of the iterator without moving it

--- src/main.rs
+++ src/main.rs
@@ -41,4 +41,8 @@ fn main() {
             total_events
         });
     println!("{:#?}", combined_events);
+
+    let mut number_iter = [7, 8, 9, 10].into_iter();
+    let first_two = number_iter.by_ref().take(2).collect::<Vec<_>>();
+    let second_two = number_iter.take(2).collect::<Vec<_>>();
 }
