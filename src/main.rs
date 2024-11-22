9fa3eb47d09409683c9c0be6be3629925a6c000b
zimrustpro <zimrustpro@gmail.com>
2024-11-22 22:24:52 +0200

    Create iterator that loops forever using `.cycle()`

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,5 @@
-use core::num;
-
-fn in_char_vec(char_vec: &Vec<char>, check: char) {
-    println!(
-        "Is {} inside? {}",
-        check,
-        char_vec.iter().any(|&char| char == check)
-    );
-}
-
 fn main() {
-    let char_vec = ('a'..'働').collect::<Vec<char>>();
-    in_char_vec(&char_vec, 'i');
-    in_char_vec(&char_vec, '뷁');
-    in_char_vec(&char_vec, '鑿');
-
-    let smaller_vec = ('A'..'z').collect::<Vec<char>>();
-    println!(
-        "All alphabetic? {}",
-        smaller_vec.iter().all(|x| x.is_alphabetic())
-    );
-    println!(
-        "All less than the character 행? {}",
-        smaller_vec.iter().all(|&x| x < '행')
-    );
-
-    let mut big_vec = vec![6; 1000];
-    big_vec.push(5);
-
-    let mut iterator = big_vec.iter().rev();
-    assert_eq!(iterator.next(), Some(&5));
-    assert_eq!(iterator.next(), Some(&6));
-
-    println!("{:?}", big_vec.iter().rev().any(|&number| number == 5));
-
-    let mut big_vec = vec![6; 1000];
-    big_vec.push(5);
-
-    let mut num_loops = 0;
-    let mut big_iter = big_vec.into_iter();
-    loop {
-        num_loops += 1;
-        if big_iter.next() == Some(5) {
-            break;
-        }
-    }
-    println!("Number of loops: {}", num_loops);
-
-    let num_vec = vec![10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
-    println!("{:?}", num_vec.iter().find(|num| *num % 3 == 0));
-    println!("{:?}", num_vec.iter().position(|num| *num % 3 == 0));
-    println!("{:?}", num_vec.iter().find(|num| *num % 3 == 11));
-    println!("{:?}", num_vec.iter().position(|num| *num % 3 == 11));
+    let even_odd_iter = ["even", "odd"].into_iter().cycle();
+    let even_odd_vec = (0..=5).zip(even_odd_iter).collect::<Vec<(i32, &str)>>();
+    println!("{:?}", even_odd_vec);
 }
