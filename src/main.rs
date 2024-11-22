b049a79df65b68d69e1c699a2092e8f74cda69ab
zimrustpro <zimrustpro@gmail.com>
2024-11-22 23:01:57 +0200

    Create a peekable iterator

--- src/main.rs
+++ src/main.rs
@@ -64,4 +64,14 @@ fn main() {
     for (index, item) in some_str.match_indices(". ") {
         println!("{} at {}", item, index);
     }
+
+    let just_numbers = vec![1, 5, 100];
+    let mut number_iter = just_numbers.iter().peekable();
+
+    for _ in 0..3 {
+        println!("I love the number {}", number_iter.peek().unwrap());
+        println!("I really love the number {}", number_iter.peek().unwrap());
+        println!("{} is such a nice number", number_iter.peek().unwrap());
+        number_iter.next();
+    }
 }
